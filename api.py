16d443f90b9eedc2dcdb7a776461560c1ed5422f
Piyush Dawadi <124043340+piyushdawadi@users.noreply.github.com>
2024-03-05 19:29:16 +0545

    final change api

--- api.py
+++ api.py
@@ -1,32 +1,42 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from sklearn.impute import SimpleImputer
 import joblib
 import pandas as pd
 
 # Create Flask App
+# Create Flask App
 app = Flask(__name__)
+CORS(app, origins='*')
 
-
-# Connect Post API Call ----> predict() function
-@app.route('/predict', methods=['POST'])
-def predict():
-
-    # Get JSON Request
-    feat_data = request.json
-
-    # Convert JSON to Pandas DF (col names)
-    df =pd.DataFrame(feat_data)
-    df = df.reindex(columns = col_names)
-
-    # Predict
-    prediction = list(model.predict(df))
-
-    return jsonify({'prediction':str(prediction)})
 
 # Load model and load column names
+model = joblib.load('final_model.pkl')
+col_names = joblib.load('column_names.pkl')
+
+# Connect Post API Call ----> predict() function
+@app.route('/prediction', methods=['POST'])
+def predict():
+    try:
+        # Get JSON Request
+        feat_data = request.json
+        feat_series = pd.Series(feat_data)
+
+        # Convert JSON to Pandas DF (col names)
+        df = pd.DataFrame([feat_series])
+        imputer = SimpleImputer(strategy='mean')
+        df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+        df = df.reindex(columns = col_names)
+
+        # Predict
+        prediction = list(model.predict(df_imputed))
+
+        return jsonify({'prediction':str(prediction)})
+    
+    except Exception as e:
+        return jsonify({'error':str(e)}), 500
+
 
 if __name__ == '__main__':
-
-    model = joblib.load('final_model.pkl')
-    col_names = joblib.load('column_names.pkl')
 
     app.run(debug=True)
